2e72f4eda5454cb55a2addbb702da19503fb379e
sisbas <sisbas@yahoo.com>
2016-05-13 14:07:27 +0300

    Home page and jobs page designed

---
+++ SmartProIK/Controllers/MeslekController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartProIK.Controllers
+{
+    public class MeslekController : Controller
+    {
+        // GET: Meslek
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ElemanAra()
+        {
+            return View();
+        }
+    }
+}
