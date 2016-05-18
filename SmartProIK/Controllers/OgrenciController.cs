d2caf4ade0fad36d3066363a7a86a7ba1f592b5b
sisbas <sisbas@yahoo.com>
2016-05-18 10:54:16 +0300

    last

--- SmartProIK/Controllers/OgrenciController.cs
+++ SmartProIK/Controllers/OgrenciController.cs
@@ -9,7 +9,15 @@ namespace SmartProIK.Controllers
     public class OgrenciController : Controller
     {
         // GET: Ogrenci
+        public ActionResult Index()
+        {
+            return View();
+        }
         public ActionResult Profil()
+        {
+            return View();
+        }
+        public ActionResult ProfilDuzenleme()
         {
             return View();
         }
